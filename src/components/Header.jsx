b9b764f6cb8e52eb71dfd0d3cc3d5e2d2d186663
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2025-10-19 15:42:53 +0800

    feat(header): add series of span for themes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
+import logo from "../assets/logo.png";
+
 export default function Header() {
 	return (
 		<header>
 			<div className="logo">
-				
+				<img src={logo} alt="logo" />
+				<span>Task-App</span>
+			</div>
+			<div className="theme-selector">
+				<span className="light active-theme"></span>
+				<span className="medium"></span>
+				<span className="dark"></span>
+				<span className="gOne"></span>
+				<span className="gTwo"></span>
+				<span className="gThree"></span>
 			</div>
 		</header>
 	);
